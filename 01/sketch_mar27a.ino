02cff95aed607adbd7b86ff9689041cababb1a22
KimChangKyun <45621009+KimChangKyun@users.noreply.github.com>
2019-03-28 19:38:26 +0900

    ver 0.2
    배열을 사용하는 것으로 소스수정

--- 01/sketch_mar27a.ino
+++ 01/sketch_mar27a.ino
@@ -1,5 +1,6 @@
 /*
- * 본예제는 LED 포트번호가 순차정렬이 되어있다는 가정하에 작성되었습니다.
+ * 본예제는 LED 포트번호가 순차정렬이 되어있다는 가정하에 주석을 이해하기 쉽게
+ * 1234포트의 LED를 사용한다는 가정하에 주석을 사용하였습니다.
  */
 int pinLED[] = {0,1,2,3}; //LED 포트 번호
 int k;
@@ -17,7 +18,7 @@ void loop() {
   //LED개수만큼 FOR문
    for (k=0; k<=forLast; k++) { 
     //01 ~ 06까지 적용하고싶은 함수 선택
-    step02(k); 
+    step06(k); 
   } 
 }
 
@@ -28,13 +29,13 @@ void loop() {
  */
 void step01(int pin){
    //LED 모두 켜줌
-    digitalWrite(pin,HIGH);
+    digitalWrite(pinLED[pin],HIGH);
 
     //받아온 LED포트번호와 총 LED 개수가 같다면 모두 꺼줌
     if(pin == forLast){
       delay(dTime);
       for(int k_in=0; k_in<=forLast; k_in++) {
-        digitalWrite(k_in,LOW);
+        digitalWrite(pinLED[k_in],LOW);
       }
       delay(dTime);
     }    
@@ -47,12 +48,12 @@ void step01(int pin){
  */
 void step02(int pin){
   //순차적으로 LED켜짐
-   digitalWrite(pin,HIGH);
+   digitalWrite(pinLED[pin],HIGH);
     delay(dTime);
     //받아온 LED포트번호와 총 LED 개수가 같다면 순차적으로 하나씩 꺼짐
     if(pin == forLast){
       for(int k_in=0; k_in<=forLast; k_in++) {
-        digitalWrite(k_in,LOW);
+        digitalWrite(pinLED[k_in],LOW);
         delay(dTime);
       }
     }    
@@ -65,13 +66,13 @@ void step02(int pin){
  */
 void step03(int pin){
   //순차적으로 LED켜짐
-    digitalWrite(pin,HIGH);
+    digitalWrite(pinLED[pin],HIGH);
     delay(dTime);
     //받아온 LED포트번호와 총 LED 개수가 같다면 역순차적으로 하나씩 꺼짐
     if(pin == forLast){  
       while(pin>=0) {
         //pin-- 1차감 연산자(처음에 맨 마지막 번호를 꺼야하기때문에 뒤에다 --를 붙여줌)
-        digitalWrite(pin--,LOW); 
+        digitalWrite(pinLED[pin--],LOW); 
         delay(dTime);
       }
     }
@@ -85,9 +86,9 @@ void step03(int pin){
  * LOGIC : [HIGH]1.[LOW]1.[HIGH]2.[LOW]2 ~~~
  */
 void step04(int pin){
-   digitalWrite(pin,HIGH);
+   digitalWrite(pinLED[pin],HIGH);
     delay(dTime);
-    digitalWrite(pin,LOW);
+    digitalWrite(pinLED[pin],LOW);
     delay(dTime);  
 }
 
@@ -105,17 +106,17 @@ void step05(int pin){
   
   if(pin%2==1){ //2의 배수가 아니면
     for(int k_in=(half+1); k_in<=forLast; k_in++) {
-      digitalWrite(k_in,LOW); //3,4 꺼짐
+      digitalWrite(pinLED[k_in],LOW); //3,4 꺼짐
     }
     for(int k_in=0; k_in<=half; k_in++) {
-      digitalWrite(k_in,HIGH); //1,2 꺼짐
+      digitalWrite(pinLED[k_in],HIGH); //1,2 꺼짐
     }
   }else{ //2의 배수가  맞으면
     for(int k_in=0; k_in<=half; k_in++) {
-      digitalWrite(k_in,LOW); //1,2 꺼짐
+      digitalWrite(pinLED[k_in],LOW); //1,2 꺼짐
     }
     for(int k_in=(half+1); k_in<=forLast; k_in++) {
-      digitalWrite(k_in,HIGH); //3,4 켜짐
+      digitalWrite(pinLED[k_in],HIGH); //3,4 켜짐
     }
   } 
   delay(dTime);
@@ -136,12 +137,12 @@ void step06(int pin){
   }
   delay(dTime);
   if(forLast != pin){ //마지막이 아니면
-    digitalWrite(pin_pre,LOW); //이전 LED를 꺼준다
-    digitalWrite(pin,HIGH); //현재의 LED를 켜준다.
-    digitalWrite(pin_next,HIGH); //다음의 LED를 켜준다.
+    digitalWrite(pinLED[pin_pre],LOW); //이전 LED를 꺼준다
+    digitalWrite(pinLED[pin],HIGH); //현재의 LED를 켜준다.
+    digitalWrite(pinLED[pin_next],HIGH); //다음의 LED를 켜준다.
   }else{ //마지막이면
-    digitalWrite(pin,LOW); //현재의 LED를 꺼준다
-    digitalWrite(pin_pre,LOW); //이전의 LED를 꺼준다
+    digitalWrite(pinLED[pin],LOW); //현재의 LED를 꺼준다
+    digitalWrite(pinLED[pin_pre],LOW); //이전의 LED를 꺼준다
   }
   delay(dTime);
 }
